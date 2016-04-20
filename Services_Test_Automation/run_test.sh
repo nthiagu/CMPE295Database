c5a8c5fd98b5f6918cdccb0ec23523db03a8006c
nthiagu <nthiagu@gmail.com>
2016-04-20 12:46:04 -0700

    Multi run Output and other run output
    Thiagu - Multi run output & other output 04/20/2016

--- Services_Test_Automation/run_test.sh
+++ Services_Test_Automation/run_test.sh
@@ -1,5 +1,2 @@
 newman -c ShopSmartCustomer_Final.json -p -O OUT_ShopSmartCustomer_$(date +%Y%m%d-%H%M%S).json
 newman -c ShopSmartAdmin_Final.json -p -O OUT_ShopSmartAdmin_$(date +%Y%m%d-%H%M%S).json
-
-#newman -c ShopSmartCustomer_Final.json -n 5 -p -O OUT_ShopSmartCustomer_Final.json
-#newman -c ShopSmartAdmin_Final.json -n 5 -p -O OUT_ShopSmartAdmin_Final.json
